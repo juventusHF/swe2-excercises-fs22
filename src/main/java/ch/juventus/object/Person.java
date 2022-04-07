465cef50657fcec773d9bb70a7327ce00050e8dd
Linda Krueger <lkrueger17@gmail.com>
2022-04-07 17:02:11 +0200

    Add collections exercise solution

--- src/main/java/ch/juventus/object/Person.java
+++ src/main/java/ch/juventus/object/Person.java
@@ -18,6 +18,11 @@ public class Person {
         this.hasPets = hasPets;
     }
 
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
     public String getFirstName() {
         return firstName;
     }
