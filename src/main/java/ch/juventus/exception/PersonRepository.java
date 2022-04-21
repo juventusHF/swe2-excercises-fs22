4eb35b7c028b6b267037cafa9967cbc2917a78c5
Linda Krueger <lkrueger17@gmail.com>
2022-04-21 19:16:18 +0200

    change exception message

--- src/main/java/ch/juventus/exception/PersonRepository.java
+++ src/main/java/ch/juventus/exception/PersonRepository.java
@@ -5,11 +5,11 @@ import ch.juventus.object.Person;
 public class PersonRepository {
 
     public Person findById(int id) throws PersonNotFoundException {
-        throw new PersonNotFoundException("oh noooo!");
+        throw new PersonNotFoundException("Could not find person with id " + id);
     }
 
     public Person addPerson(Person person) {
-        throw new IllegalArgumentException("Given person could not be added!");
+        throw new IllegalArgumentException("oh noooo!");
     }
 
 }
